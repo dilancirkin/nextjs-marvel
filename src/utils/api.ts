f98b927c6cce584b786073bb7492cdba56ba96ad
dilancirkin <dilancirkin@gmail.com>
2024-02-21 01:32:10 +0300

    data fetching was done

---
+++ src/utils/api.ts
@@ -0,0 +1,33 @@
+import { CharacterWrapper } from "../models/types";
+import md5 from 'md5';
+
+const URL = 'https://gateway.marvel.com/v1/public';
+const PUBLIC_KEY= '01f80549575103b56a658fbe6b67dd17';
+const PRIVATE_KEY = `be664ea704d82cc2646d41b606481f42dec80792`;
+
+const getTimeStamp = () => Date.now().toString();
+const getHash = (timeStamp: string) => md5(timeStamp+PRIVATE_KEY+PUBLIC_KEY);
+
+const timeStamp = getTimeStamp();
+const hash = getHash(timeStamp);
+const query = `ts=${timeStamp}&apikey=${PUBLIC_KEY}&hash=${hash}`
+
+const handleResponse = async <T>(response: Response) => {
+    if(!response.ok) {
+        throw new Error(response.statusText)
+      }
+  const data = await response.json();
+  return data.data as T;
+}
+
+export const getCharacters = async (): Promise<CharacterWrapper> => {
+  const url = `${URL}/characters?${query}`;
+  const response = await fetch(url);
+  return handleResponse<CharacterWrapper>(response);
+}
+export const detailCharacter = async (characterId: string): Promise<CharacterWrapper> => {
+    const url = `${URL}/characters/${characterId}?${query}`;
+    const response = await fetch(url);
+    return handleResponse<CharacterWrapper>(response);
+  }
+  
